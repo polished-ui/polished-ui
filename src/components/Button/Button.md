52e1ed7030b8f20b6b3255aff790c4ca2c294b82
dbarabashdev <dbarabashdev@gmail.com>
2020-01-28 11:52:42 +0200

    added basic button component

--- src/components/Button/Button.md
+++ src/components/Button/Button.md
@@ -1,3 +1,3 @@
 ```jsx
-<Button>Here</Button>
+<Button default>Here</Button>
 ```
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import styled from "styled-components"
 
-const Button = ({ children }) => <button>{children}</button>
+const StyledButton = styled.button`
+  border: 0;
+  background-color: ${props => (props.default ? "#CCC" : props.primary ? "#90caf9" : null)};
+`
+
+const Button = ({ children, type }) => <StyledButton type>{children}</StyledButton>
 
 export default Button
