cc68f518ff0f19a31e0625fbc119393ebbe3373a
dbarabashdev <dbarabashdev@gmail.com>
2020-01-28 15:40:21 +0200

    added button styled element

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,18 @@
-import React from "react"
 import styled from "styled-components"
 
-const StyledButton = styled.button`
-  border: 0;
-  background-color: ${props => (props.default ? "#CCC" : props.primary ? "#90caf9" : null)};
+const Button = styled.button`
+  width: ${props => props.width + `px`};
+  height: ${props => props.height + `px`};
+  background-color: ${props => props.backgroundColor};
+  border: ${props => props.border};
+  color: ${props => props.textColor};
+  font-size: ${props => props.sizeText + `px`};
+  border-radius: ${props => props.radius + `px`};
+  cursor: pointer;
+  &:hover {
+    background-color: ${props => props.hoverColor};
+  }
 `
 
-const Button = ({ children, type }) => <StyledButton type>{children}</StyledButton>
-
+/** @component */
 export default Button
